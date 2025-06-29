3bdc021d7acdcbb44b2823d753d7df984dee1603
Ankur Gurjar <76687403+8ankur8@users.noreply.github.com>
2025-06-30 00:49:10 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -172,7 +172,7 @@ function App() {
         >
           Start Playing Free
         </button>
-        <p className="text-sm [font-family:'Caveat',Helvetica] font-bold text-gray-500 mt-4">
+        <p className="text-lg [font-family:'Caveat',Helvetica] font-bold text-gray-500 mt-4">
           Join thousands of players worldwide
         </p>
       </section>
