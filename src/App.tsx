de150cfa1a55b7141a70a793ee9fac6d0a828003
Ankur Gurjar <76687403+8ankur8@users.noreply.github.com>
2025-06-30 03:51:24 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -168,7 +168,7 @@ function App() {
     <div className="absolute inset-0">
       {/* First overlay image */}
       <img
-        className="absolute w-[40%] sm:w-[40%] h-auto top-[65%] left-[-1%] z-10"
+        className="absolute w-[40%] sm:w-[40%] h-auto top-[65%] left-[-2%] z-10"
         alt="Image"
         src="/image-11.png"
       />
