c920140942f0d3e282f2a8e5cf51c8c2d79f5b3d
Ankur Gurjar <76687403+8ankur8@users.noreply.github.com>
2025-06-30 09:24:53 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -340,7 +340,7 @@ function App() {
 
   {/* Pricing Cards */}
   <div id="pricing" className="max-w-7xl mx-auto mt-[30px] sm:mt-[40px]">
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6">
+    <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 sm:gap-6">
       {pricingPlans.map((plan, index) => (
         <Card
           key={index}
