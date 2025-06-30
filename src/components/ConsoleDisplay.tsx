e7dcca45f91f332448c693cce9b3d911e5fc3cb5
Ankur Gurjar <76687403+8ankur8@users.noreply.github.com>
2025-06-30 09:58:06 +0530

    Updated ConsoleDisplay.tsx

--- src/components/ConsoleDisplay.tsx
+++ src/components/ConsoleDisplay.tsx
@@ -637,7 +637,7 @@ const ConsoleDisplay: React.FC = () => {
   }
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-gray-900 via-purple-950 to-indigo-900 text-white">
+    <div className="min-h-screen  text-white">
       {/* Header */}
       <header className="p-4 ">
         <div className="container mx-auto flex justify-between items-center">
